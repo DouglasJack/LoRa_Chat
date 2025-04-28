6bf86a60a4c35127908806bfb14ec47e22d1db17
Jack Nolen <douglasjack888@gmail.com>
2025-04-28 14:58:36 -0600

    Added training

--- Messenger.py
+++ Messenger.py
@@ -1,3 +1,4 @@
+import threading
 from os.path import exists
 
 import Comm
@@ -15,6 +16,8 @@ def MessageToCodes(msg):
             return "Head is not AT or String command"
         case "+ERR=4":
             return "Unkown command"
+        case "+ERR=5":
+            return "Data length does not match length of message"
         case "+ERR=10":
             return "TX is over times?"
         case "+ERR=12":
@@ -38,13 +41,20 @@ def MessageToCodes(msg):
 
 class Messenger:
     def __init__(self, sport):
+        self.myAddress = 0
         self.comm = Comm.Comm(sport, self)
         self.messageCache = []
         self.lastMessageSent = None
         self.clearToSend = False  # IF ENABLED, No messages can be sent
         self.clearToSendIssueTime = None
+<<<<<<< Updated upstream
+=======
+        time.sleep(1)  # This is required. (Used to setup thread in COMM)
+
+>>>>>>> Stashed changes
         self.tr = Protocols.Training.Training(self)
-        self.tr.searching()
+        self.trainingThread = threading.Thread(target=self.tr.searching, daemon=True)
+        self.trainingThread.start()
 
     def RecievedMessage(self, msg):
         # Converts message serial string into Messenger object.
@@ -59,21 +69,28 @@ class Messenger:
         else:
             # Should be a recieved message.
             MsgPacket = Message.Message()
-            
+
             MsgPacket = MsgPacket.recievedMessage(msg)
-            if MsgPacket.ascii_to_binary(MsgPacket.flag)[3] == 1:
+            if MsgPacket.ascii_to_binary(MsgPacket.flag)[2] == "1":
                 # Address bit is raised, handle accordingly.
                 self.tr.received(MsgPacket)
-                
+                return
+
             MsgPacket.recievedMessage(msg)
             # For WebUI, only cache if it's from another device
             if MsgPacket.encryption:
                 self.messageCache.append(MsgPacket)
 
+<<<<<<< Updated upstream
    def CustomMessage(self, Message):
+=======
+    # When using CustomMessage, its expected you handle the packet creation yourself.
+    def CustomMessage(self, Message, ignoreCTS=False):
+>>>>>>> Stashed changes
         # Must pass Message class
-        self.lastMessageSent = Message
-        self.comm.send(Message.data)
+        if ignoreCTS or not self.clearToSend:
+            self.lastMessageSent = Message
+            self.comm.send(Message.data, True)
 
     def ChatMessage(self, msg):
         if self.clearToSend and self.clearToSendIssueTime:
@@ -84,5 +101,5 @@ class Messenger:
         MsgPacket.newMessage(msg)
 
         # Creates a message packet, then sends it. The message will generate the correct data for sending the command.
-        self.lastMessageSent = MsgPacket #Last message sent
+        self.lastMessageSent = MsgPacket  #Last message sent
         self.comm.send(MsgPacket.data)

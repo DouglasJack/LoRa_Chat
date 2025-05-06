d7b5ac4cd73970917410ba1fc53fc2fee36aaed8
cgarriv <122755821+cgarriv@users.noreply.github.com>
2025-05-06 03:40:10 -0600

    Update encryption_key.py
    HMAC key

--- encryption_key.py
+++ encryption_key.py
@@ -1,5 +1,7 @@
 # Need to pip install cryptography
 from cryptography.fernet import Fernet
 
-FERNET_KEY = b'KEY GOES HERE' # Insert the shared key
+FERNET_KEY = b'6aCI7dAGXJxrpVPXuhOoB4zS_szxcoNJr8q1_S4Hl6E=' # Insert the shared key
 cipher = Fernet(FERNET_KEY)
+
+hmac_key = b'cMm\x80j%.\xe3\x93\n\xb7Q\xf1T\x96\xff\x1e4|\xe6\x97R\xf5\xfb\xe6\x98\n\xa8\xbd$b\x8c'
